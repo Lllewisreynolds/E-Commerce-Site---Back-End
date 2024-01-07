bcbbfcdb1de63e6bcead83bf1a365666e92abca6
Lllewisreynolds <lsreynolds108@gmail.com>
2024-01-07 14:48:10 +0000

    Environment variable file made functional (removed EXAMPLE from file name), .gitignore created, dependencies installed. Immediate starting of express server on specified port replaced with Sequelize functionality - now synchronising database models first and then starting the server in server.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+/* JawsDB as seen in prior student mini project - add-on that provides functional MySQL server
+ if you are deploying application to Heroku. Was provided as starter code but not needed for
+ video walkthrough */
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+// Also holding the configuration and connection details for database 
+
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+/* sync sequelize models to the database using below method, then turn on the server 
+using promise - only after database is synced does it start the express application
+ensures database schema is up-to-date before handling any requests */ 
+
+// Using force: false option to instruct Sequelize only to alter existing tables if there are differences
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
